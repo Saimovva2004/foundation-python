2884095ebef8741d2240d7df81c8b96c92fe7471
Saimovva2004 <144653415+Saimovva2004@users.noreply.github.com>
2026-01-22 21:58:50 +0530

    Day 02: Variables and core data types with business examples

---
+++ day02_variables/variables.py
@@ -0,0 +1,17 @@
+# Business example: a startup selling a subscription
+
+company_name = "FinTrack"
+monthly_price = 499
+gst_rate = 0.18
+is_active_customer = True
+
+print(company_name)
+print(monthly_price)
+print(gst_rate)
+print(is_active_customer)
+
+print(type(company_name))
+print(type(monthly_price))
+print(type(gst_rate))
+print(type(is_active_customer))
+
